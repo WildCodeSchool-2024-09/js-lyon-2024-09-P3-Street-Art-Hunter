b3d1ef96e1ec04f33da4130740e64f2a65ec3d77
BlueSetbyeol <setbyeol.c@gmail.com>
2024-12-18 15:57:21 +0100

    mise en place de la Navbar et du responsive sur le site entier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,26 @@
 import "./App.css";
+import Logo from "./assets/images/cc_logo_spotless_mustard.png";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <>
-      <h1>Hello World</h1>
+      <nav>
+        <img src={Logo} alt="logoCC" className="logo" />
+        <Navbar />
+      </nav>
+      <main>
+        <img src={Logo} alt="logoCC" className="logo" />
+        <h1>Hello World</h1>
+      </main>
+      <div className="narrow-nav">
+        <Navbar />
+      </div>
+      <footer>
+        <p>Créé avec </p>
+        <h3>passion</h3>
+        <p> par la Team City Canvas</p>
+      </footer>
     </>
   );
 }
---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import "./Navbar.css";
+import Connection from "../../assets/images/connec_ash.png";
+import Leadboard from "../../assets/images/lead_ash.png";
+
+export default function Navbar() {
+  return (
+    <div className="navbar-icon">
+      <hr />
+      <img src={Leadboard} alt="leadboard" />
+      <hr />
+      <img src={Connection} alt="connection" />
+      <hr />
+    </div>
+  );
+}
