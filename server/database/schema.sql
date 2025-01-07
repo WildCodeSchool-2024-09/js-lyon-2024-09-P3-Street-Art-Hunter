7e4023e541f1b96b86ed59d75df085533e1ff4f3
BlueSetbyeol <setbyeol.c@gmail.com>
2025-01-07 14:39:19 +0100

    finalisation du schema DB

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,21 +1,36 @@
-create table user (
-  id int unsigned primary key auto_increment not null,
-  email varchar(255) not null unique,
-  password varchar(255) not null
+CREATE TABLE artwork
+	( id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+	  address VARCHAR(250) NOT NULL,
+      image VARCHAR(300) NOT NULL,
+      picture_date date,
+      type_of_art VARCHAR(100),
+      latitude INT NOT NULL,
+      longitude INT NOT NULL,
+      picture_credit VARCHAR(100) NOT NULL
+	);
+    
+    INSERT INTO artwork (id,name,address,image,type_of_art,latitude,longitude,picture_credit)
+	VALUES (1,'Regards', '39 Rue Paul Chenavard, 69001 Lyon, France', 'https://www.street-artwork.com/uploads/document/5cd2897d5e44d570844651.jpg', 'tag',45.765236,4.833110,'Rabot'),
+    (2,'Le Combattant', '4 Place des Tapis, 69004 Lyon, France', 'https://www.street-artwork.com/uploads/document/5ce1a3947064a105728305.jpg','wall painting',45.775196, 4.830133,'Rabot');
+    
+ CREATE TABLE artist
+ (id INT AUTO_INCREMENT PRIMARY KEY,
+ name VARCHAR(100)
+ );
+ 
+ INSERT INTO artist (id,name)
+	VALUES (1,'inconnu'),
+    (2,'Kalouf (Blast)');
+
+CREATE TABLE creation
+(creation_date DATE,
+id_artist INT NOT NULL,
+id_artwork INT NOT NULL,
+FOREIGN KEY (id_artwork) REFERENCES artwork (id) ON DELETE NO ACTION ON UPDATE NO ACTION,
+FOREIGN KEY (id_artist) REFERENCES artist (id) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
-create table item (
-  id int unsigned primary key auto_increment not null,
-  title varchar(255) not null,
-  user_id int unsigned not null,
-  foreign key(user_id) references user(id)
-);
-
-insert into user(id, email, password)
-values
-  (1, "jdoe@mail.com", "123456");
-
-insert into item(id, title, user_id)
-values
-  (1, "Stuff", 1),
-  (2, "Doodads", 1);
+ INSERT INTO creation (creation_date,id_artwork,id_artist)
+	VALUES ('2025-01-02', 1, 1),
+    ('2022-04-07', 2, 2);
