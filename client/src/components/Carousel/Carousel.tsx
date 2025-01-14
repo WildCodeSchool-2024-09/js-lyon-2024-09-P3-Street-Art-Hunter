2cd01ceba5575ef935dbcb30562dde8df6434a4f
michaelgirardet <michaelgirardet@outlook.com>
2025-01-14 19:32:26 +0100

    created carousel, fill databases

--- client/src/components/Carousel/Carousel.tsx
+++ client/src/components/Carousel/Carousel.tsx
@@ -21,11 +21,17 @@ function Carousel() {
     autoplaySpeed: 3000,
     responsive: [
       {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
         breakpoint: 768,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
-          fade: true,
         },
       },
     ],
