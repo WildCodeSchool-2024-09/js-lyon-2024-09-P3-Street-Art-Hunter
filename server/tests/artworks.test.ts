505a1827cb57ffe7ba4a8212295f7b95b834ad2a
BlueSetbyeol <setbyeol.c@gmail.com>
2025-01-15 11:57:56 +0100

    préparation pour 2ème test

--- server/tests/artworks.test.ts
+++ server/tests/artworks.test.ts
@@ -27,3 +27,40 @@ describe("GET /api/artworks", () => {
     expect(response.body).toStrictEqual(rows);
   });
 });
+
+// // Test suite for the GET /api/items/:id route
+// describe("GET /api/items/:id", () => {
+//     it("should fetch a single item successfully", async () => {
+//       // Mock rows returned from the database
+//       const rows = [{}] as Rows;
+
+//       // Mock the implementation of the database query method
+//       jest
+//         .spyOn(databaseClient, "query")
+//         .mockImplementation(async () => [rows, []]);
+
+//       // Send a GET request to the /api/items/:id endpoint
+//       const response = await supertest(app).get("/api/items/1");
+
+//       // Assertions
+//       expect(response.status).toBe(200);
+//       expect(response.body).toStrictEqual(rows[0]);
+//     });
+
+//     it("should fail on invalid id", async () => {
+//       // Mock empty rows returned from the database
+//       const rows = [] as Rows;
+
+//       // Mock the implementation of the database query method
+//       jest
+//         .spyOn(databaseClient, "query")
+//         .mockImplementation(async () => [rows, []]);
+
+//       // Send a GET request to the /api/items/:id endpoint with an invalid ID
+//       const response = await supertest(app).get("/api/items/0");
+
+//       // Assertions
+//       expect(response.status).toBe(404);
+//       expect(response.body).toEqual({});
+//     });
+//   });
